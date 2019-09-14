aca71f89e525f95add3a2a2330085ab90c4d3a3c
Николай <enottoff@gmail.com>
2019-09-14 22:43:38 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,10 +3,8 @@
   $client = $_SERVER['HTTP_USER_AGENT']; //получаем идентификатор HTTP клиента
   $today = date("Y.m.d H:i:s"); //получаем текущие дату и время
   $f = fopen("log.csv","a"); //открываем файл для добавления данных
-  $temp = $_REQUEST['temp']; //получаем значение посланной переменной "а"
-  $pres = $_REQUEST['pres']; //получаем значение посланной переменной "а"
-  $hum = $_REQUEST['hum']; //получаем значение посланной переменной "а"
-  fwrite($f,"$today; $ip; $client; temp=$temp; pres=$pres; hum=$hum; \r\n-----------------\r\n"); //запись данных в файл
+  $param = $_REQUEST['a']; //получаем значение посланной переменной "а"
+  fwrite($f,"$today; $ip; $client; sensor=$param\r\n-----------------\r\n"); //запись данных в файл
   fclose($f); //закрываем файл
 ?>
 <p>GPRS data read page</p>
